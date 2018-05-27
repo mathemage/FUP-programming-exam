580b9334ab88cb9344abab03f739b84fd21ccc72
Karel Ha <mathemage@gmail.com>
2018-05-27 18:55:13 +0200

    Reorder means by difficulty
    - in `Means.hs`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- exam_1/4_means/Means.hs
+++ exam_1/4_means/Means.hs
@@ -7,19 +7,19 @@ lengthAsDouble = fromIntegral . length
 minMean :: [Double] -> Double
 minMean = minimum
 
--- https://en.wikipedia.org/wiki/Geometric_mean
-geometricMean :: [Double] -> Double
-geometricMean doubles = product doubles ** (1 / lengthAsDouble doubles)
+maxMean :: [Double] -> Double
+maxMean = maximum
 
 arithmeticMean :: [Double] -> Double
 arithmeticMean doubles = sum doubles / lengthAsDouble doubles
 
+-- https://en.wikipedia.org/wiki/Geometric_mean
+geometricMean :: [Double] -> Double
+geometricMean doubles = product doubles ** (1 / lengthAsDouble doubles)
+
 -- https://en.wikipedia.org/wiki/Root_mean_square
 quadraticMean :: [Double] -> Double
 quadraticMean doubles = ( (sum . map (** 2) $ doubles) / lengthAsDouble doubles ) ** 0.5
-
-maxMean :: [Double] -> Double
-maxMean = maximum
 
 -- testLine   = "1 2 39.3 2 3 -12 3.1415 -3.1415"
 testLine   = "1 2 3 4"
