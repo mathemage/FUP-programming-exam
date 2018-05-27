3b4db32a0de625ba715e1eac2ab4345e1fbd2dd3
Karel Ha <mathemage@gmail.com>
2018-05-27 18:20:34 +0200

    Define `minMean`
    - in `Means.hs`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- exam_1/4_means/Means.hs
+++ exam_1/4_means/Means.hs
@@ -1,14 +1,21 @@
 stringToFloats :: String -> [Float]
 stringToFloats line = map read (words line)
 
+minMean :: [Float] -> Float
+minMean = minimum
+
 maxMean :: [Float] -> Float
 maxMean = maximum
 
-testLine =  "1 2 39.3 2 3 -12 3.1415 -3.1415"
+-- testLine   = "1 2 39.3 2 3 -12 3.1415 -3.1415"
+testLine   = "1 2 3 4"
 testFloats = stringToFloats testLine
 
 main :: IO ()
 main = do
-  line <- getLine
-  print . stringToFloats $ line
+--   line <- getLine
 --   print . stringToFloats $ line
+  print testFloats
+  print $ "min == " ++ (show . minMean $ testFloats)
+  print $ "max == " ++ (show . maxMean $ testFloats)
+--   print . maxMean $ testFloats
