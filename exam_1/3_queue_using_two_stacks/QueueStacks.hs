a78eb12850050c22762c146969e3906ae017ed43
Karel Ha <mathemage@gmail.com>
2018-05-27 15:09:49 +0200

    Implement `pop` in `QueueStacks.hs`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- exam_1/3_queue_using_two_stacks/QueueStacks.hs
+++ exam_1/3_queue_using_two_stacks/QueueStacks.hs
@@ -9,9 +9,10 @@ module QueueStacks where
   top []    = Nothing
   top (x:_) = Just x
 
---   pop :: Stack v -> Stack v
---   pop []          = Nothing
---   pop (top:stack) =
+  pop :: Stack v -> Stack v
+  pop []           = []
+  pop (_:newStack) = newStack
+
 
 --   type Queue v = (Stack v, Stack v)
 --
@@ -21,14 +22,22 @@ module QueueStacks where
 --
 --   dispatch :: Queue v -> Queue v
 
-  -- Tests
-  stack0 :: Stack Int
-  stack0 = push 0 []
 
-  stack1 :: Stack Int
-  stack1 = push 1 stack0
+  -- Tests
+  emptyStack :: Stack Int
+  emptyStack = []
+
+  stack0 :: Stack Int
+  stack0 = push 0 emptyStack
+
+  stack01 :: Stack Int
+  stack01 = push 1 stack0
 
   completeStack :: Stack Int
-  completeStack = push 7 . push 6 . push 5 . push 4 . push 3 . push 2 . push 1 . push 0 $ []
+  completeStack = push 7 . push 6 . push 5 . push 4 . push 3 . push 2 . push 1 . push 0 $ emptyStack
 
-  topResults = map top [stack0, stack1, completeStack]
+  stack42 :: Stack Int
+  stack42 = push 7 . push 6 . push 5 . push 42 . pop . pop . pop . pop $ completeStack
+
+  listOfStacks = [emptyStack, stack0, stack01, completeStack, stack42]
+  topResults = map top listOfStacks
