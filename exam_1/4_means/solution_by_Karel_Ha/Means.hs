cea46ca383f8a2242ca278dd5fb9be5a1dc0ff35
Karel Ha <mathemage@gmail.com>
2018-05-30 23:08:25 +0200

    Re-order means
    -in `exam_1/4_means`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- exam_1/4_means/solution_by_Karel_Ha/Means.hs
+++ exam_1/4_means/solution_by_Karel_Ha/Means.hs
@@ -5,20 +5,20 @@ stringToDoubles line = map read (words line)
 lengthAsDouble :: [a] -> Double
 lengthAsDouble = fromIntegral . length
 
-arithmeticMean :: [Double] -> Double
-arithmeticMean doubles = sum doubles / lengthAsDouble doubles
+-- https://en.wikipedia.org/wiki/Harmonic_mean
+harmonicMean :: [Double] -> Double
+harmonicMean doubles = lengthAsDouble doubles / (sum . map (1/) $ doubles)
 
 -- https://en.wikipedia.org/wiki/Geometric_mean
 geometricMean :: [Double] -> Double
 geometricMean doubles = product doubles ** (1 / lengthAsDouble doubles)
 
+arithmeticMean :: [Double] -> Double
+arithmeticMean doubles = sum doubles / lengthAsDouble doubles
+
 -- https://en.wikipedia.org/wiki/Root_mean_square
 quadraticMean :: [Double] -> Double
 quadraticMean doubles = ( (sum . map (** 2) $ doubles) / lengthAsDouble doubles ) ** 0.5
-
--- https://en.wikipedia.org/wiki/Harmonic_mean
-harmonicMean :: [Double] -> Double
-harmonicMean doubles = lengthAsDouble doubles / (sum . map (1/) $ doubles)
 
 -- testLine   = "1 2 3 4"
 testLine   = "1 2 39.3 2 3 -12 3.1415 -3.1415"
