327447c45c07dedf336f8aa4c94eeba1acdf7c81
Lasse Blaauwbroek <LasseBlaauwbroek@users.noreply.github.com>
2018-06-19 20:44:16 +0200

    Exam 3 solution 4

---
+++ Exam3/4-index-list-sum/Lasse/q4.hs
@@ -0,0 +1,33 @@
+-- Solution 1
+
+wordIndex :: [Int] -> Int -> Int
+wordIndex words n = i
+  where
+    (c, i) = foldl f (0, 0) words
+    f (c, i) m | c + m + 1 > n = (c + m + 1, i)
+               | otherwise     = (c + m + 1, i + 1)
+
+countUntil (' ':s) = 1 + countUntil s
+countUntil _ = 0
+
+sumNatN ls n = sum . take (ls !! n) . drop (n + 1) . cycle $ ls
+
+main :: IO ()
+main = do s1 <- getLine
+          s2 <- getLine
+          let nums = words s1
+          let wi = (wordIndex (map length nums) (countUntil s2))
+          putStrLn . show . sumNatN (map read nums) $ wi
+
+-- Solution 2
+
+solve :: [String] -> Int -> Int -> Int
+solve (s:ls) acc n | acc + l + 1 > n = sum . map read . take i $ ls
+                   | otherwise       = solve ls (acc + l + 1) n
+                   where l = length s
+                         i = read s
+
+main2 :: IO ()
+main2 = do s1 <- getLine
+           s2 <- getLine
+           putStrLn . show $ solve (cycle . words $ s1) 0 (countUntil s2)
